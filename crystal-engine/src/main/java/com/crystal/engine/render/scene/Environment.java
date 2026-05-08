8ef1ea48d90c58e556cecb09f6b3883971c3eb7e
Dragonslayer6710 <74316385+Dragonslayer6710@users.noreply.github.com>
2026-05-08 17:21:43 +0100

    Environment Update!

--- crystal-engine/src/main/java/com/crystal/engine/render/scene/Environment.java
+++ crystal-engine/src/main/java/com/crystal/engine/render/scene/Environment.java
@@ -1,5 +1,6 @@
 package com.crystal.engine.render.scene;
 
+import com.crystal.engine.render.texture.Texture;
 import org.joml.Vector3f;
 
 public class Environment {
@@ -7,12 +8,33 @@ public class Environment {
     private final Vector3f ambientColor = new Vector3f(0.03f);
     private float ambientIntensity = 1.0f;
 
+    private Texture skybox;
+    private Texture irradianceMap;
+    private Texture prefilteredEnvironmentMap;
+    private Texture brdfLut;
+
     public Vector3f getAmbientColor() {
         return ambientColor;
     }
 
     public float getAmbientIntensity() {
         return ambientIntensity;
+    }
+
+    public Texture getSkybox() {
+        return skybox;
+    }
+
+    public Texture getIrradianceMap() {
+        return irradianceMap;
+    }
+
+    public Texture getPrefilteredEnvironmentMap() {
+        return prefilteredEnvironmentMap;
+    }
+
+    public Texture getBrdfLut() {
+        return brdfLut;
     }
 
     public Environment setAmbientColor(float r, float g, float b) {
@@ -23,5 +45,35 @@ public class Environment {
     public Environment setAmbientIntensity(float intensity) {
         this.ambientIntensity = intensity;
         return this;
+    }
+
+    public Environment setSkybox(Texture skybox) {
+        this.skybox = skybox;
+        return this;
+    }
+
+    public Environment setIrradianceMap(Texture irradianceMap) {
+        this.irradianceMap = irradianceMap;
+        return this;
+    }
+
+    public Environment setPrefilteredEnvironmentMap(Texture prefilteredEnvironmentMap) {
+        this.prefilteredEnvironmentMap = prefilteredEnvironmentMap;
+        return this;
+    }
+
+    public Environment setBrdfLut(Texture brdfLut) {
+        this.brdfLut = brdfLut;
+        return this;
+    }
+
+    public boolean hasSkybox() {
+        return skybox != null;
+    }
+
+    public boolean hasIBL() {
+        return irradianceMap != null
+                && prefilteredEnvironmentMap != null
+                && brdfLut != null;
     }
 }
